97323d86f4c0d4d5b07df998b6d55b550f0b75de
Syawqi Muhammad <syawqi.zone@gmail.com>
2019-05-20 14:18:11 +0800

    https

--- analyze.php
+++ analyze.php
@@ -15,7 +15,7 @@ if (isset($_POST['submit'])) {
 <html>
 <head>
     <title>Analyze Sample</title>
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
 </head>
 <body>
  
